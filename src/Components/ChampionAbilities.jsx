135f05403d7bccc50739b519f40ea632329a1d20
feittanJC <josuecastillom07@gmail.com>
2023-07-16 23:35:05 -0500

    cuarto commit

--- src/Components/ChampionAbilities.jsx
+++ src/Components/ChampionAbilities.jsx
@@ -33,13 +33,13 @@ function ChampionAbilities() {
 
   return (
     <div className="flex justify-center pt-24 pb-8">
-      <div className="flex justify-start w-[65%]">
+      <div className="flex justify-start w-[60%]">
         <img
           src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champId}_0.jpg`}
           alt=""
         />
-        <div className="pl-5">
-          <div className="text-white  py-8">
+        <div className="pl-5 w-full">
+          <div className="text-white  py-12">
             <h1 className="text-3xl pb-4">Pasiva</h1>
             <div className="flex items-center pl-4">
               <img
@@ -51,53 +51,71 @@ function ChampionAbilities() {
               </p>
             </div>
           </div>
-          <div className="text-white">
+          <div className="text-white w-auto pt-12">
             <h1 className="text-3xl">Habilidades</h1>
-            <div className="flex items-center pl-4 pt-4">
-              <img
-                src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/spell/${
-                    data && data.spells && data.spells[0] ? data.spells[0].id : ""
+            <div className="flex justify-between ">
+              <div className="flex items-center pl-4 pt-8 w-1/2">
+                <img
+                  src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/spell/${
+                    data && data.spells && data.spells[0]
+                      ? data.spells[0].id
+                      : ""
                   }.png`}
-                alt=""
-              />
-              <p className="pl-8 text-lg">
-                {data && data.spells && data.spells ? data.spells[0].name : ""}
-              </p>
-            </div>
-            <div className="flex items-center pl-4 pt-4">
-              <img
-               src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/spell/${
-                data && data.spells && data.spells[1] ? data.spells[1].id : ""
-              }.png`}
-                alt=""
-              />
-              <p className="pl-8 text-lg">
-                {data && data.spells && data.spells ? data.spells[1].name : ""}
-              </p>
-            </div>
-            <div className="flex items-center pl-4 pt-4">
-              <img
-                src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/spell/${
-                    data && data.spells && data.spells[2] ? data.spells[2].id : ""
+                  alt=""
+                />
+                <p className="pl-8 text-lg">
+                  {data && data.spells && data.spells
+                    ? data.spells[0].name
+                    : ""}
+                </p>
+              </div>
+              <div className="flex items-center pl-4 pt-8 w-1/2">
+                <img
+                  src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/spell/${
+                    data && data.spells && data.spells[1]
+                      ? data.spells[1].id
+                      : ""
                   }.png`}
-                alt=""
-              />
-              <p className="pl-8 text-lg">
-                {data && data.spells && data.spells ? data.spells[2].name : ""}
-              </p>
+                  alt=""
+                />
+                <p className="pl-8 text-lg">
+                  {data && data.spells && data.spells
+                    ? data.spells[1].name
+                    : ""}
+                </p>
+              </div>
             </div>
-            <div className="flex items-center pl-4 pt-4">
-              <img
-                src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/spell/${
-                  data && data.spells && data.spells[3] ? data.spells[3].id : ""
-                }.png`}
-                alt=""
-              />
-              <p className="pl-8 text-lg">
-                {data && data.spells && data.spells[3]
-                  ? data.spells[3].name
-                  : ""}
-              </p>
+            <div className="flex justify-between ">
+              <div className="flex items-center pl-4 pt-4 w-1/2">
+                <img
+                  src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/spell/${
+                    data && data.spells && data.spells[2]
+                      ? data.spells[2].id
+                      : ""
+                  }.png`}
+                  alt=""
+                />
+                <p className="pl-8 text-lg">
+                  {data && data.spells && data.spells
+                    ? data.spells[2].name
+                    : ""}
+                </p>
+              </div>
+              <div className="flex items-center pl-4 pt-4 w-1/2">
+                <img
+                  src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/spell/${
+                    data && data.spells && data.spells[3]
+                      ? data.spells[3].id
+                      : ""
+                  }.png`}
+                  alt=""
+                />
+                <p className="pl-8 text-lg">
+                  {data && data.spells && data.spells[3]
+                    ? data.spells[3].name
+                    : ""}
+                </p>
+              </div>
             </div>
           </div>
         </div>
